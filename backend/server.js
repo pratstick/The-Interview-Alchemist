3edd25376602931624400e0f9d92b085569d8ba9
pratstick <116508117+pratstick@users.noreply.github.com>
2025-05-16 21:29:52 +0530

    ver 0.3.1

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes"); 
+const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
 const { protect } = require("./middlewares/authMiddleware");
@@ -11,8 +11,7 @@ const { generateInterviewQuestions, generateConceptExplanation } = require("./co
 
 const app = express();
 
-//Middleware to handle CORS
-
+// Middleware to handle CORS
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -21,22 +20,29 @@ app.use(cors({
 
 connectDB();
 
-//Middleware 
+// Middleware
 app.use(express.json());
 
-//Routes
+// Routes
 app.use("/api/auth", authRoutes);
-app.use('/api/sessions', sessionRoutes);
+app.use("/api/sessions", sessionRoutes);
 app.use("/api/questions", questionRoutes);
 
 app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-app.use("/api/ai/generate-explanation", protect, generateConceptExplanation); 
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
-//Serve uploads folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads"),{}));
+// Serve frontend (production)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
-//Start server
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
