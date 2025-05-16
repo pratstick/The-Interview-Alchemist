6f131827c44fa011fc4d3132b19d6d6e311c1cf9
pratstick <116508117+pratstick@users.noreply.github.com>
2025-05-16 22:20:05 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 // Serve frontend (production)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/the-interview-alchemist/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/the-interview-alchemist/dist", "index.html"));
   });
 }
 
